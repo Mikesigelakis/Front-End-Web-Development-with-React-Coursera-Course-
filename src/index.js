083b2078d7ce64162651bf0cefa440352c222cec
Michail Sygkelakis <mikesglks@gmail.com>
2020-05-04 06:35:58 +0300

    Header and Footer

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import App from "./App";
+import "font-awesome/css/font-awesome.css";
+import "bootstrap-social/bootstrap-social.css";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render( <
-    React.StrictMode >
-    <
-    App / >
-    <
-    /React.StrictMode>,
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
     document.getElementById("root")
 );
 
